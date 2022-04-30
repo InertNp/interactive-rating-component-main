1e25859b1d0422056990799222f936390b726433
Abhi Shek <40143810+InertNp@users.noreply.github.com>
2022-04-30 20:33:27 +0545

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const ratingTxt = document.getElementById("ratingTxt")
 const rating = 0;
 con2.style.display = "none";
 function handleValue(e) {
-    ratingTxt.innerHTML = "You selected "+e+" out of 5";
+    ratingTxt.innerHTML = `You selected ${e} out of 5`;
 }
 function handleSubmit() {
     con1.style.display = "none"
